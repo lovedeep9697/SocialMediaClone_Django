19f34bff7675fd43a0f5b7fe8db3f088937cf3ff
lovedeep <lovedeepsingh9697@gmail.com>
2018-06-16 12:38:24 +0530

    accounts complete

--- SocialMediaClone/accounts/views.py
+++ SocialMediaClone/accounts/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
 from . import forms
 from django.views.generic import CreateView
 
@@ -11,5 +11,5 @@ from django.views.generic import CreateView
 class SignUp(CreateView):
 	form_class = forms.UserCreateForm
 	success_url = reverse_lazy('login')
-	template_name = 'accounts/sigup.html'
+	template_name = 'accounts/signup.html'
 
