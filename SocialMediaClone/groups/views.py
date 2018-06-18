954f0a72846e0ac93c486fc32567165534d744ba
lovedeep <lovedeepsingh9697@gmail.com>
2018-06-18 17:25:45 +0530

    group detail added

--- SocialMediaClone/groups/views.py
+++ SocialMediaClone/groups/views.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.contrib.auth import LoginRequiredMixin,PermissionRequiredMixin
+from djando.url import reverse
+from django.views import generic
+from groups.models import Group,GroupMember
 # Create your views here.
+
+
+class CreateGroup(LoginRequiredMixin,generic.CreateView):
+	fields = ('name','description')
+	model = Group
+
+class SingleGroup(generic.DetailView):
+	model = Group
+
+class ListGroups(generic.ListView):
+	models = Group
+
