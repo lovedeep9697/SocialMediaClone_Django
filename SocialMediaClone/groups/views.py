b437f87897ffa4900da7dbb7c776a691e011c63b
lovedeep <lovedeepsingh9697@gmail.com>
2018-06-27 16:10:30 +0530

    join and leave

--- SocialMediaClone/groups/views.py
+++ SocialMediaClone/groups/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth import LoginRequiredMixin,PermissionRequiredMixin
 from djando.url import reverse
 from django.views import generic
 from groups.models import Group,GroupMember
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
 # Create your views here.
 
 
@@ -18,4 +20,52 @@ class SingleGroup(generic.DetailView):
 
 class ListGroups(generic.ListView):
 	models = Group
+
+class JoinGroup(LoginRequiredMixin,generic.RedirectView):
+
+	def get_redirect_url(self,*args,**kwargs):
+		return reverse('groups:single',kwargs={'slug':self.kwargs.get('slug')})
+
+	def get(self,request,*args,**kwargs):
+		group = get_object_or_404(Group,slug=self.kwargs.get('slug'))
+
+		try:
+			GroupMember.objects.create(user = self.request.user,group=group)
+		except IntegrityError:
+			messages.warning(self.request,'Warning, Already a member')
+		else:
+			messaged.success(self.request,'You are now a member')
+
+		return super().get(request,*args,**kwargs)
+
+
+
+
+
+class LeaveGroup(LoginRequiredMixin,generic.RedirectView):
+
+
+	def get_redirect_url(self,*args,**kwargs):
+		return reverse('groups:single',kwargs={'slug':self.kwargs.get('slug')})
+
+	def get(self,request,*args,**kwargs):
+
+		try:
+			membership = models.GroupMember.objects.filter(
+				 user = self.request.user,
+				 group__slug = self.kqargs.get('slug')
+
+				).get()
+
+		except models.GroupMember.DoesNotExist:
+			messages.warning(self.request,'Sorry you are not in this group')
+
+		else:
+			membership.delete()
+			messages.success(self.request,'you have left the group')
+
+		return super().get(request,*args,**kwargs)
+
+
+
 
